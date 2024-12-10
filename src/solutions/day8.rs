c0d7adb51eb0e23de733c0f25fd2eee5b0d348e6
Nasonov V <qxuken@qxc-mbp-m3.lan>
2024-12-11 00:33:20 +0300

    feat: add day8 solution

--- src/solutions/day8.rs
+++ src/solutions/day8.rs
@@ -12,9 +12,51 @@ use crate::{
     solutions::utils::Coord,
 };
 
+#[instrument(ret(level = Level::TRACE))]
+fn calc_antinodes_coords(
+    (f_row, f_col): &Coord,
+    (s_row, s_col): &Coord,
+    bounds: isize,
+) -> Vec<Coord> {
+    let mut res = vec![];
+    if f_row == s_row && f_col == s_col {
+        return res;
+    }
+    let v_row = *s_row as isize - *f_row as isize;
+    let v_col = *s_col as isize - *f_col as isize;
+
+    trace!(v = ?(v_row, v_col));
+    let mut n_row = *s_row as isize + v_row;
+    let mut n_col = *s_col as isize + v_col;
+    while 0 <= n_row.min(n_col) && n_row.max(n_col) < bounds {
+        trace!(n = ?(n_row, n_col));
+        res.push((n_row as usize, n_col as usize));
+        n_row += v_row;
+        n_col += v_col;
+    }
+    res
+}
+
+#[instrument(ret(level = Level::TRACE))]
+fn calc_antinodes_coord((f_row, f_col): &Coord, (s_row, s_col): &Coord) -> Option<Coord> {
+    if f_row == s_row && f_col == s_col {
+        return None;
+    }
+
+    let v_row = *s_row as isize - *f_row as isize;
+    let v_col = *s_col as isize - *f_col as isize;
+
+    let n_row = *s_row as isize + v_row;
+    let n_col = *s_col as isize + v_col;
+
+    trace!(v = ?(v_row, v_col));
+
+    usize::try_from(n_row).ok().zip(usize::try_from(n_col).ok())
+}
+
 pub fn solve(second: bool, line_reader: impl Iterator<Item = io::Result<String>>) -> Result<()> {
     if second {
-        try_combine_numbers_with_concat(line_reader)?;
+        count_antinodes_rec(line_reader)?;
     } else {
         count_antinodes(line_reader)?;
     }
@@ -25,37 +67,152 @@ pub fn solve(second: bool, line_reader: impl Iterator<Item = io::Result<String>>
 fn count_antinodes(
     line_reader: impl Iterator<Item = io::Result<String>>,
 ) -> Result<usize, AppError> {
-    let lines = line_reader
+    let mut char_map = line_reader
         .map_ok(|s| s.chars().collect())
         .collect::<io::Result<Vec<Vec<char>>>>()
         .map_err(|e| AppError::DataParse(e.to_string()))?;
 
-    let stations = lines.iter().enumerate().fold(
-        HashMap::<char, HashSet<Coord>>::new(),
-        |mut acc, (row_i, row)| {
-            row.iter()
-                .enumerate()
-                .filter(|(_, &ch)| ch != '.')
-                .for_each(|(col_i, ch)| {
-                    acc.entry(*ch)
-                        .and_modify(|s| {
-                            s.insert((row_i, col_i));
-                        })
-                        .or_insert(HashSet::from([(col_i, row_i)]));
-                });
-            acc
-        },
-    );
-    trace!(stations = ?stations, "Parsed");
+    let stations: HashMap<char, HashSet<Coord>> =
+        char_map
+            .iter()
+            .enumerate()
+            .fold(HashMap::new(), |mut acc, (row_i, row)| {
+                row.iter()
+                    .copied()
+                    .enumerate()
+                    .filter(|(_, ch)| ch != &'.')
+                    .for_each(|(col_i, ch)| {
+                        acc.entry(ch)
+                            .and_modify(|s| {
+                                s.insert((row_i, col_i));
+                            })
+                            .or_insert(HashSet::from([(row_i, col_i)]));
+                    });
+                acc
+            });
 
-    Ok(0)
+    trace!(char_map = ?char_map, stations = ?stations, "Parsed");
+
+    let antinodes = stations
+        .par_iter()
+        .flat_map(|(_l, coords)| {
+            coords
+                .par_iter()
+                .flat_map(|first_coord| {
+                    coords
+                        .par_iter()
+                        .filter_map(|second_coord| calc_antinodes_coord(first_coord, second_coord))
+                        .collect::<Vec<Coord>>()
+                })
+                .collect::<Vec<Coord>>()
+        })
+        .filter(|(row, col)| row.max(col) < &char_map.len())
+        .collect::<Vec<Coord>>()
+        .into_iter()
+        .unique()
+        .collect_vec();
+
+    for (row, col) in antinodes.iter() {
+        if char_map[*row][*col] == '.' {
+            char_map[*row][*col] = '#';
+        }
+    }
+
+    let mut map = String::with_capacity(char_map.len().pow(2) + char_map.len());
+    for row in char_map.iter() {
+        map.push_str(&row.iter().join(""));
+        map.push('\n');
+    }
+
+    trace!(antinodes = ?antinodes, "Antinodes calculated");
+    trace!("New map\n{map}");
+
+    Ok(antinodes.len())
 }
 
 #[instrument(skip_all, ret)]
-fn try_combine_numbers_with_concat(
-    _line_reader: impl Iterator<Item = io::Result<String>>,
+fn count_antinodes_rec(
+    line_reader: impl Iterator<Item = io::Result<String>>,
 ) -> Result<usize, AppError> {
-    unimplemented!()
+    let mut char_map = line_reader
+        .map_ok(|s| s.chars().collect())
+        .collect::<io::Result<Vec<Vec<char>>>>()
+        .map_err(|e| AppError::DataParse(e.to_string()))?;
+
+    let coords: HashSet<Coord> =
+        char_map
+            .iter()
+            .enumerate()
+            .fold(HashSet::new(), |mut acc, (row_i, row)| {
+                row.iter()
+                    .copied()
+                    .enumerate()
+                    .filter(|(_, ch)| ch != &'.')
+                    .for_each(|(col_i, _ch)| {
+                        acc.insert((row_i, col_i));
+                    });
+                acc
+            });
+
+    let stations: HashMap<char, HashSet<Coord>> =
+        char_map
+            .iter()
+            .enumerate()
+            .fold(HashMap::new(), |mut acc, (row_i, row)| {
+                row.iter()
+                    .copied()
+                    .enumerate()
+                    .filter(|(_, ch)| ch != &'.')
+                    .for_each(|(col_i, ch)| {
+                        acc.entry(ch)
+                            .and_modify(|s| {
+                                s.insert((row_i, col_i));
+                            })
+                            .or_insert(HashSet::from([(row_i, col_i)]));
+                    });
+                acc
+            });
+
+    trace!(char_map = ?char_map, coords = ?coords, stations = ?stations, "Parsed");
+
+    let bounds = char_map.len() as isize;
+    let antinodes = stations
+        .par_iter()
+        .flat_map(|(_l, coords)| {
+            coords
+                .par_iter()
+                .flat_map(|first_coord| {
+                    coords
+                        .par_iter()
+                        .flat_map(|second_coord| {
+                            calc_antinodes_coords(first_coord, second_coord, bounds)
+                        })
+                        .collect::<Vec<Coord>>()
+                })
+                .collect::<Vec<Coord>>()
+        })
+        .filter(|coord| !coords.contains(coord))
+        .collect::<Vec<Coord>>()
+        .into_iter()
+        .unique()
+        .collect_vec();
+
+    for (row, col) in antinodes.iter() {
+        if char_map[*row][*col] == '.' {
+            char_map[*row][*col] = '#';
+        }
+    }
+
+    let mut map = String::with_capacity(char_map.len().pow(2) + char_map.len());
+    for row in char_map.iter() {
+        map.push_str(&row.iter().join(""));
+        map.push('\n');
+    }
+
+    trace!(antinodes = ?antinodes, "Antinodes calculated");
+    trace!("New map\n{map}");
+
+    Ok(antinodes.len() + coords.len())
 }
 
 #[cfg(test)]
@@ -65,34 +222,37 @@ mod test {
     #[test]
     fn validate_one_star_example() {
         let data = r#"............
-                            ........0...
-                            .....0......
-                            .......0....
-                            ....0.......
-                            ......A.....
-                            ............
-                            ............
-                            ........A...
-                            .........A..
-                            ............
-                            ............"#;
+                      ........0...
+                      .....0......
+                      .......0....
+                      ....0.......
+                      ......A.....
+                      ............
+                      ............
+                      ........A...
+                      .........A..
+                      ............
+                      ............"#;
         let res = count_antinodes(data.lines().map(|s| s.trim().to_string()).map(Ok));
         assert_eq!(res.unwrap(), 14);
     }
 
     #[test]
     fn validate_second_star_example() {
-        let data = r#"190: 10 19
-                      3267: 81 40 27
-                      83: 17 5
-                      156: 15 6
-                      7290: 6 8 6 15
-                      161011: 16 10 13
-                      192: 17 8 14
-                      21037: 9 7 18 13
-                      292: 11 6 16 20"#;
-        let res =
-            try_combine_numbers_with_concat(data.lines().map(|s| s.trim().to_string()).map(Ok));
-        assert_eq!(res.unwrap(), 11387);
+        let data = r#"............
+                      ........0...
+                      .....0......
+                      .......0....
+                      ....0.......
+                      ......A.....
+                      ............
+                      ............
+                      ........A...
+                      .........A..
+                      ............
+                      ............"#;
+
+        let res = count_antinodes_rec(data.lines().map(|s| s.trim().to_string()).map(Ok));
+        assert_eq!(res.unwrap(), 34);
     }
 }
