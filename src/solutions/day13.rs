f64ca0d0a0c2ac6e39bdb62c3db99c5614f42538
Nasonov V <qxuken@QXC-MBP-M3.lan>
2024-12-24 00:01:53 +0300

    feat: add day13 reduce computation

--- src/solutions/day13.rs
+++ src/solutions/day13.rs
@@ -17,7 +17,7 @@ struct Machine {
 }
 
 impl Machine {
-    #[instrument(ret(level = Level::TRACE))]
+    #[instrument(ret(level = Level::DEBUG))]
     fn min_tokens(&self, max_presses: usize) -> Option<usize> {
         let mut cheap = self.b_diff;
         let mut costy = self.a_diff;
@@ -26,44 +26,58 @@ impl Machine {
             mem::swap(&mut cheap, &mut costy);
         }
         let mut presses = (
-            ((self.target.0 / cheap.0).max(self.target.1 / cheap.1) + 1).min(max_presses),
+            (self.target.0 / cheap.0)
+                .min(self.target.1 / cheap.1)
+                .min(max_presses),
             0,
         );
         let x_coord = |presses: &(usize, usize)| presses.0 * cheap.0 + presses.1 * costy.0;
         let y_coord = |presses: &(usize, usize)| presses.0 * cheap.1 + presses.1 * costy.1;
-        if presses.0 == max_presses {
-            while x_coord(&presses) < self.target.0
-                && y_coord(&presses) < self.target.1
-                && presses.1 < 100
-            {
-                presses.1 += 1;
-            }
-        }
+        let mut curr_target = (x_coord(&presses), y_coord(&presses));
         trace!(
             reversed,
             ?presses,
+            ?curr_target,
             x_coord = x_coord(&presses),
             y_coord = y_coord(&presses)
         );
-        while presses.0 > 0
-            && presses.1 < max_presses
-            && !(x_coord(&presses) == self.target.0 && y_coord(&presses) == self.target.1)
-        {
+        presses.1 = ((self.target.0 - curr_target.0) / costy.0)
+            .min((self.target.1 - curr_target.1) / costy.1)
+            .min(max_presses);
+        curr_target.0 += costy.0 * presses.1;
+        curr_target.1 += costy.1 * presses.1;
+        trace!(
+            reversed,
+            ?presses,
+            ?curr_target,
+            x_coord = x_coord(&presses),
+            y_coord = y_coord(&presses)
+        );
+        while presses.0 > 0 && curr_target != self.target {
+            // trace!(
+            //     ?presses,
+            //     ?curr_target,
+            //     sub_x = cheap.0 + costy.0 * presses.1,
+            //     sub_y = cheap.1 + costy.1 * presses.1,
+            // );
             presses.0 -= 1;
-            while x_coord(&presses) < self.target.0
-                && y_coord(&presses) < self.target.1
-                && presses.1 < max_presses
-            {
-                presses.1 += 1;
-            }
+            curr_target.0 -= cheap.0 + costy.0 * presses.1;
+            curr_target.1 -= cheap.1 + costy.1 * presses.1;
+
+            presses.1 = ((self.target.0 - curr_target.0) / costy.0)
+                .min((self.target.1 - curr_target.1) / costy.1)
+                .min(max_presses);
+            curr_target.0 += costy.0 * presses.1;
+            curr_target.1 += costy.1 * presses.1;
         }
         trace!(
             reversed,
             ?presses,
+            ?curr_target,
             x_coord = x_coord(&presses),
             y_coord = y_coord(&presses)
         );
-        if x_coord(&presses) == self.target.0 && y_coord(&presses) == self.target.1 {
+        if curr_target == self.target {
             if reversed {
                 Some(presses.0 * 3 + presses.1)
             } else {
@@ -75,9 +89,10 @@ impl Machine {
     }
 
     #[instrument]
-    fn fix_target_coord(&mut self, val: usize) {
+    fn fix_target_coord(mut self, val: usize) -> Self {
         self.target.0 += val;
         self.target.1 += val;
+        self
     }
 }
 
@@ -156,15 +171,13 @@ fn task_hard(line_reader: impl Iterator<Item = io::Result<String>>) -> Result<us
             let a_diff = parse_line_into_usize_tuple(&lines[0]).unwrap();
             let b_diff = parse_line_into_usize_tuple(&lines[1]).unwrap();
             let target = parse_line_into_usize_tuple(&lines[2]).unwrap();
-            let mut machine = Machine {
+            Machine {
                 a_diff,
                 b_diff,
                 target,
-            };
-            machine.fix_target_coord(10000000000000);
-
-            machine
+            }
         })
+        .map_ok(|m| m.fix_target_coord(10000000000000))
         .collect::<io::Result<Vec<Machine>>>()
         .map_err(|e| AppError::DataParse(e.to_string()))?;
     trace!(machines = ?machines, count = machines.len());
@@ -200,6 +213,15 @@ mod test {
                       Prize: X=18641, Y=10279"#;
         let res = task_simple(data.lines().map(|s| s.trim().to_string()).map(Ok));
         assert_eq!(res.unwrap(), 480);
+    }
+
+    #[test]
+    fn validate_failed_case_1() {
+        let data = r#"Button A: X+21, Y+30
+                      Button B: X+87, Y+34
+                      Prize: X=4413, Y=1790"#;
+        let res = task_simple(data.lines().map(|s| s.trim().to_string()).map(Ok));
+        assert_eq!(res.unwrap(), 59);
     }
 
     #[test]
